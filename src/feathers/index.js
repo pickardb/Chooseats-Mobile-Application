340dc4b24755a7d47bb133d4ea235a32ce50fa44
pickardb <blakepickard@hotmail.com>
2018-11-15 13:39:19 -0800

    Committing changes before implementing frontend testing

--- src/feathers/index.js
+++ src/feathers/index.js
@@ -3,7 +3,7 @@ import socketio from '@feathersjs/socketio-client';
 import auth from '@feathersjs/authentication-client';
 import io from 'socket.io-client';
 
-const socket = io('http://206.87.153.59:3030/');
+const socket = io('http://10.200.1.118:3030/');
 
 // Configure feathers-client
 const feathersClient = feathers()
